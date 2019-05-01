546f9c7ec81c44093ae17d970ca4c95a22af4d63
Mozgopraw1 <mr.devgen@gmail.com>
2019-05-01 13:13:05 +0500

    golangci-lint run проверка и правильное оформление bool

--- gotest.go
+++ gotest.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"math/rand"
 	"time"
-	"golint"
 )
 
 func main() {
@@ -18,7 +17,7 @@ func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
 
 	// создание массива
-	xo := make([]int, 9, 9)
+	xo := make([]int, 9)
 
 	// значение точки для хода
 	var x int
@@ -28,20 +27,19 @@ func main() {
 }
 
 //основная функция по запуску игры
-func xod (xo[]int, x int, flag bool, flagIf bool) {
+func xod(xo []int, x int, flag bool, flagIf bool) {
 	for i := 0; i <= 8; i++ {
-		x = rand.Intn(8) 				// рандомная цифра от 0 до 8
-		vievXo(xo, flag, flagIf, x)         // показ поля
-		xoVariant(flag, x, xo, flagIf) 		// проверка хода
-		xoStageAssign(xo, x, flag)          // функция присвоения
+		x = rand.Intn(8)            // рандомная цифра от 0 до 8
+		vievXo(xo, flag, flagIf, x)    // показ поля
+		xoVariant(flag, x, xo, flagIf) // проверка хода
+		xoStageAssign(xo, x, flag)     // функция присвоения
 
-
-		flag1(&flag)						// смена стороны
+		flag1(&flag) 					// смена стороны
 	}
 }
 
 //вывод поля
-func vievXo(xo[]int, flag bool, flagIf bool, x int){
+func vievXo(xo []int, flag bool, flagIf bool, x int) {
 	fmt.Println(xo[0], xo[1], xo[2])
 	fmt.Println(xo[3], xo[4], xo[5])
 	fmt.Println(xo[6], xo[7], xo[8])
@@ -51,8 +49,8 @@ func vievXo(xo[]int, flag bool, flagIf bool, x int){
 }
 
 //смена хода
-func flag1(flag *bool, ) {
-	if *flag == true {
+func flag1(flag *bool) {
+	if *flag{
 		*flag = false
 	} else {
 		*flag = true
@@ -61,14 +59,14 @@ func flag1(flag *bool, ) {
 
 // ход на поле
 // проверка чей ход
-func xoVariant(flag bool, x int, xo[]int, flagIf bool) {
+func xoVariant(flag bool, x int, xo []int, flagIf bool) {
 	// X ход
-	if flag == true {
+	if flag{
 		xoCor(xo, &x, &flagIf)
 		flagIf = false
 	}
 	// O ход
-	if flag == false {
+	if flag{
 		xoCor(xo, &x, &flagIf)
 		flagIf = false
 	}
@@ -77,27 +75,28 @@ func xoVariant(flag bool, x int, xo[]int, flagIf bool) {
 // проверка был ли уже ход в данной точке
 // я запутался в указателях именно тут, все тесты показывают что X - как точка куда ставится X или O
 // не меняется вне функции и в последующей функции, но в самой функции вроде как меняется (вроде даже в xoCorDop тоже)
-func xoCor(xo[] int, x *int, flagIf *bool) {
+func xoCor(xo []int, x *int, flagIf *bool) {
 	if xo[*x] == 0 {
 		*flagIf = true
 	}
-	if *flagIf == false {
+	if !*flagIf {
 		xoCorDop(xo, x, flagIf)
 	}
 }
+
 // если точка уже занята
-func xoCorDop(xo[] int, x *int, flagIf *bool) {
+func xoCorDop(xo []int, x *int, flagIf *bool) {
 	*flagIf = false
 	*x = rand.Intn(8) // сразу даёт рандомное значение
 	xoCor(xo, x, flagIf)
 }
 
 //функция присвоения значения проверки
-func xoStageAssign(xo[] int, x int, flag bool) {
-	if flag == true {		//ход X
+func xoStageAssign(xo []int, x int, flag bool) {
+	if flag{ //ход X
 		xo[x] = 1
 	}
-	if flag == false {		//ход O
+	if !flag{ //ход O
 		xo[x] = 2
 	}
 }
