a845e357c0cba0d695fd8877ec5fa7a840aeaa77
Mozgopraw1 <mr.devgen@gmail.com>
2019-05-05 19:57:33 +0500

    По диагонали пришлось разделить на две функции, работают они отлично, можно попробовать впихнуть в основную программу теперь

--- testingModule.go
+++ testingModule.go
@@ -18,17 +18,19 @@ func main() {
 		xo[1] = 1
 		xo[2] = 1
 		xo[3] = 1
-		xo[4] = 2
+		xo[4] = 1
 		xo[5] = 1
-		xo[6] = 2
+		xo[6] = 1
 		xo[7] = 1
-		xo[8] = 1
+		xo[8] = 2
 		var check1 int
 		//выполнения функции
 		//flagWinG := winGoriz(rx, xo, check1)
 		//fmt.Println(flagWinG)
-		flagWinV := winVertic(rx, xo, check1)
-		fmt.Println("flagWinV: ", flagWinV)
+		flagWinD1 := winDiag1(rx, xo, check1)
+		fmt.Println("flagWinD1: ", flagWinD1)
+		flagWinD2 := winDiag2(rx, xo, check1)
+		fmt.Println("flagWinD2: ", flagWinD2)
 	}
 }
 
@@ -74,10 +76,36 @@ func winVertic(rx int, xo []int, check1 int)int{
 	return 0
 }
 
-//winDiag == выполнение
-//func winDiag(rx int, xo []int, check1 int)int{
+//winDiag1 == выполнение сравнения по диагонали сверху в низ слева на право
+func winDiag1(rx int, xo []int, check1 int)int{
+	a := 0
+	for i:=0; i <=rx-1; i++ {
+		if xo[a] == 1 {check1++}
 
-//}
+		a = a+rx+1
+	}
+	if check1 == rx{
+		return 1
+	}
+	check1 = 0
+	viewXo(xo, rx)
+	return 0
+}
+
+func winDiag2 (rx int, xo []int, check1 int)int{
+	a := rx-1
+	for i:=0; i <=rx-1; i++ {
+		if xo[a] == 1 {check1++}
+
+		a = a+rx-1
+	}
+	if check1 == rx {
+		return 1
+	}
+	check1 = 0
+	viewXo(xo, rx)
+	return 0
+}
 
 func viewXo(xo []int, rx int) {
 	t := 0
