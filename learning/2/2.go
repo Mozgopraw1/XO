507c874f455bc8e191fb0e909a3ffc5559af435c
Mozgopraw1 <mr.devgen@gmail.com>
2019-08-31 12:30:41 +0500

    0.0.1
    Отоформатировал репозиторий.

---
+++ learning/2/2.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	showMeTheMoney()
+}
+
+func showMeTheMoney()  {
+	fmt.Printf("$$$$")
+}
+
+// sum - несколько входящих значений
+func sum(i int, j int) int {
+	return i + j
+}
+
+// sumLight - упрощенная запись для нескольких значений одинакового типа
+func sumLight(i, j int) int {
+	return i + J
+}
+
